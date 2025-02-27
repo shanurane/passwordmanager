7324b8a6130a4e10cf8316f492e1f120248e0fc0
shanurane <shanurane1020@gmail.com>
2025-02-27 15:28:06 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PassMan/",
+  base: "/passwordmanager/",
   plugins: [react()],
 });
